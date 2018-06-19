23a9ae555f77ac0cbd18847458448efc152e609d
aguxez <dizmiiguel@gmail.com>
2018-06-18 21:02:52 -0400

    moved code out to give correct responsability to SC

--- contracts/CToken.sol
+++ contracts/CToken.sol
@@ -1,71 +1,58 @@
 pragma solidity ^0.4.2;
 
 import 'zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
+import 'zeppelin-solidity/contracts/token/ERC721/ERC721BasicToken.sol';
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
-contract CToken is ERC721Token, Ownable {
+contract CToken is ERC721BasicToken, ERC721Token, Ownable {
     constructor() ERC721Token("CToken", "CT") public {
         owner = msg.sender;
     }
 
-    // TODO: Check value types from this struct.
-    struct Horse {
-        uint256 saleId;
-        uint256 timestamp;
-        string bio;
-        bytes32 name;
-        bytes32 color;
-        bytes32 horseType;
-        bytes32 breed;
-        bytes32 runningStyle;
-        bytes32 origin;
-        bytes32 gender;
-        bytes32 rank;
-        bytes32 pedigree;
-        bytes32 parents;
-        bytes32 grandparents;
-        bytes32 phenotypes;
-        bytes32 genotypes;
-        bytes32 previousOwner;
-        address buyer;
-    }
+    // Mapping for owned address to horse IDs
+    mapping(address => uint256[]) ownedHorses;
 
-    Horse[] horses;
+    // Mapping from horse ID to index.
+    mapping(uint256 => uint256) ownedHorsesIndex;
+
+    /* @dev Returns an array of ids of horses owned by '_from' */
+    function _getHorsesOwned(address _from) public returns(uint256[]) {
+        return ownedHorses[_from];
+    }
 
     /*
     @param _byteParams is just a list with the elements from the struct so we don't run into compile errors.
     for having a stack too deep.
     Check if the size of the array is always 14 by comparing to struct elements. */
-    function mint(uint256 _saleId, address _buyer, string _bio, bytes32[14] _byteParams) public onlyOwner {
-        Horse memory horse = Horse({
-            saleId: _saleId,
-            timestamp: now,
-            bio: _bio,
-            buyer: _buyer,
-            name: _byteParams[0],
-            color: _byteParams[1],
-            horseType: _byteParams[2],
-            breed: _byteParams[3],
-            runningStyle: _byteParams[4],
-            origin: _byteParams[5],
-            gender: _byteParams[6],
-            rank: _byteParams[7],
-            pedigree: _byteParams[8],
-            parents: _byteParams[9],
-            grandparents: _byteParams[10],
-            phenotypes: _byteParams[11],
-            genotypes: _byteParams[12],
-            previousOwner: _byteParams[13]
-        });
-
-        uint horseId = horses.push(horse) - 1;
-
-        _mint(msg.sender, horseId);
+    function mint(address _sender, uint256 _horseId) public {
+        _mint(_sender, _horseId);
     }
 
-    function _getHorse(uint256 _horseId) public view returns(string) {
-        Horse memory horse = horses[_horseId];
+    /* @dev adds horse to list of owned horses by an address
+    Throws if the horse already exists on the user list. */
+    function _addHorse(address _to, uint256 _horseId) public onlyOwnerOf(_horseId) {
+        uint256 length = ownedHorses[_to].length;
+        ownedHorses[_to].push(_horseId);
+        ownedHorsesIndex[_horseId] = length;
+    }
 
-        return (horse.bio);
+    /* @dev We're implementing this functionality even though open-zepellin implements it
+    because we don't have the option to retrieve all the tokens owned by an address from their framework.
+    Throws if _from isn't the owner of _horseId */
+    function _removeHorse(address _from, uint256 _horseId) public onlyOwnerOf(_horseId) {
+        // We're just copying zepellin's implementation.
+        uint256 tokenIndex = ownedHorsesIndex[_horseId];
+        uint256 lastTokenIndex = ownedHorses[_from].length.sub(1);
+        uint256 lastToken = ownedHorses[_from][lastTokenIndex];
+
+        ownedHorses[_from][tokenIndex] = lastToken;
+        ownedHorses[_from][lastTokenIndex] = 0;
+        // Note that this will handle single-element arrays. In that case, both tokenIndex and lastTokenIndex are going to
+        // be zero. Then we can make sure that we will remove _horseId from the ownedHorses list since we are first swapping
+        // the lastToken to the first position, and then dropping the element placed in the last position of the list
+
+        ownedHorses[_from].length--;
+        ownedHorsesIndex[_horseId] = 0;
+        ownedHorsesIndex[lastToken] = tokenIndex;
     }
 }
