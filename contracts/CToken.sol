130422431985330edc2eccda47a025be2601d6d5
aguxez <diazmiiguel@gmail.com>
2018-06-22 18:03:17 -0400

    removes unused functions

--- contracts/CToken.sol
+++ contracts/CToken.sol
@@ -28,32 +28,4 @@ contract CToken is ERC721BasicToken, ERC721Token, Ownable {
         approve(_to, _horseId);
         safeTransferFrom(_from, _to, _horseId);
     }
-
-    /* @dev adds horse to list of owned horses by an address
-    Throws if the horse already exists on the user list. */
-    function addHorse(address _to, uint256 _horseId) public {
-        uint256 length = ownedHorses[_to].length;
-        ownedHorses[_to].push(_horseId);
-        ownedHorsesIndex[_horseId] = length;
-    }
-
-    /* @dev We're implementing this functionality even though open-zepellin implements it
-    because we don't have the option to retrieve all the tokens owned by an address from their framework.
-    Throws if _from isn't the owner of _horseId */
-    function removeHorse(address _from, uint256 _horseId) public {
-        // We're just copying zepellin's implementation.
-        uint256 tokenIndex = ownedHorsesIndex[_horseId];
-        uint256 lastTokenIndex = ownedHorses[_from].length.sub(1);
-        uint256 lastToken = ownedHorses[_from][lastTokenIndex];
-
-        ownedHorses[_from][tokenIndex] = lastToken;
-        ownedHorses[_from][lastTokenIndex] = 0;
-        // Note that this will handle single-element arrays. In that case, both tokenIndex and lastTokenIndex are going to
-        // be zero. Then we can make sure that we will remove _horseId from the ownedHorses list since we are first swapping
-        // the lastToken to the first position, and then dropping the element placed in the last position of the list
-
-        ownedHorses[_from].length--;
-        ownedHorsesIndex[_horseId] = 0;
-        ownedHorsesIndex[lastToken] = tokenIndex;
-    }
 }
