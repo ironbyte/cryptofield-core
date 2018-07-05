661d075a453ba5e6aa187a5660e8b5b6c5deab76
Miguel Diaz <diazmiiguel@gmail.com>
2018-07-05 14:29:58 -0400

    adds infura network

--- truffle.js
+++ truffle.js
@@ -1,7 +1,15 @@
 require("babel-register")({
   ignore: /node_modules\/(?!zeppelin-solidity)/
 });
+
 require("babel-polyfill");
+
+if(process.env.NODE_ENV !== "production") {
+  require("dotenv").load();
+}
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env.mnemonic
 
 module.exports = {
   solc: {
@@ -15,6 +23,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
+    },
+    ropsten: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + process.env.infura_token)
+      },
+      network_id: 3
     }
   }
 };
