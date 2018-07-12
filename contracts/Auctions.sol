33981edfff3c3ec68288d7b96673ae7143c6599e
Miguel Diaz <diazmiiguel@gmail.com>
2018-07-12 14:34:29 -0400

    getAuctionDuration and dev comments

--- contracts/Auctions.sol
+++ contracts/Auctions.sol
@@ -13,6 +13,7 @@ contract Auctions is usingOraclize {
     struct AuctionData {
         address owner;
         bool isOpen;
+        uint256 duration;
     }
 
     mapping(uint256 => AuctionData) auctions;
@@ -30,16 +31,23 @@ contract Auctions is usingOraclize {
         uint256 auctionId = auctionIds.push(0) - 1;
 
         sendAuctionQuery(_duration, auctionId);
-        auctions[auctionId] = AuctionData(_user, true);
+        auctions[auctionId] = AuctionData(_user, true, _duration);
     }
 
+    /*
+    @dev We construct the query with the auction ID and duration of it.
+    */
     function sendAuctionQuery(uint256 _duration, uint256 _auctionId) private {
-        string memory url = "json(https://2d920100.ngrok.io/api/v1/close_auction).auction_closed";
+        string memory url = "json(https://375b0412.ngrok.io/api/v1/close_auction).auction_closed";
         string memory payload = strConcat("{\"auction\":", uint2str(_auctionId), "}");
 
         oraclize_query(_duration, "URL", url, payload);
     }
 
+    /*
+    @dev Only one Query is being sent, when we get a response back we automatically
+    close the given auction.
+    */
     function __callback(bytes32 _id, string _result) public {
         require(msg.sender == oraclize_cbAddress());
 
@@ -50,15 +58,33 @@ contract Auctions is usingOraclize {
         auctions[uintResult].isOpen = false;
     }
 
+    /*
+    @return Returns the price of the Query so the contract has enough Ether when the query is sent.
+    */
     function getQueryPrice() public returns(uint) {
         return oraclize_getPrice("URL");
     }
 
+    /* 
+    @dev Check if an auction is open or closed by a given ID.
+    */
     function getAuctionStatus(uint _id) public view returns(bool) {
         return auctions[_id].isOpen;
     }
 
+    /*
+    @dev Just returns the length of all the auctions created for enumeration and tests.
+    */
     function getAuctionsLength() public view returns(uint) {
         return auctionIds.length;
+    }
+
+    /*
+    @dev Gets the duration of a given auction.
+    */  
+    function getAuctionDuration(uint256 _id) public view returns(uint256) {
+        AuctionData memory auction = auctions[_id];
+
+        return auction.duration;
     }
 }
