286136c0d847d574999b78976f382f9c6860e3d0
Miguel Diaz <diazmiiguel@gmail.com>
2018-07-13 14:06:42 -0400

    fixup

--- contracts/ERC721Token.sol
+++ contracts/ERC721Token.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.2;
 
-import 'zeppelin-solidity/contracts/token/ERC721/ERC721.sol';
-import 'zeppelin-solidity/contracts/token/ERC721/ERC721BasicToken.sol';
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721BasicToken.sol";
 
 contract ERC721Token is ERC721, ERC721BasicToken {
   // Token name
