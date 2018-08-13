06b7c5b0de5bc3223ac1fac44d7aa74a5f3f7207
Miguel Diaz <diazmiiguel@gmail.com>
2018-08-12 20:26:40 -0400

    Rename and fixup

--- migrations/2_deploy_core_contract.js
+++ migrations/2_deploy_core_contract.js
@@ -1,9 +1,9 @@
-const Auctions = artifacts.require("./Auctions.sol");
+const Core = artifacts.require("./Core.sol");
 
 module.exports = (deployer, network, acc) => {
   if(process.env.NODE_ENV === "production"|| process.env.NODE_ENV === "testnet") {
-    deployer.deploy(Auctions);
+    deployer.deploy(Core);
   } else {
-    deployer.deploy(Auctions, {from: acc[1]});
+    deployer.deploy(Core, {from: acc[1]});
   }
 }
--- migrations/3_deploy_sale_auctions_contract.js
+++ migrations/3_deploy_sale_auctions_contract.js
@@ -1,10 +1,10 @@
 const SaleAuction = artifacts.require("./SaleAuction");
-const Auctions = artifacts.require("./Auctions");
+const Core = artifacts.require("./Core");
 
 module.exports = (deployer, network, acc) => {
   if(process.env.NODE_ENV === "production"|| process.env.NODE_ENV === "testnet") {
-    deployer.deploy(SaleAuction, Auctions.address);
+    deployer.deploy(SaleAuction, Core.address);
   } else {
-    deployer.deploy(SaleAuction, Auctions.address, {from: acc[1]});
+    deployer.deploy(SaleAuction, Core.address, {from: acc[1]});
   }
 }
