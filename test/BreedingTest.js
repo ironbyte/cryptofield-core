89920d8abb517cc02d69dc1f5da8b330524ab19b
Miguel Diaz <diazmiiguel@gmail.com>
2018-08-17 00:03:52 -0400

    Breeding tests

--- test/BreedingTest.js
+++ test/BreedingTest.js
@@ -16,12 +16,7 @@ contract("Breeding", acc => {
   it("should mix two horses", async () => {
     // Mint two tokens.
     await core.createHorse(owner, "female hash"); // 1
-
-    // We add this timeout to make a space between the two horse creations
-    // so they don't have the same timestamp when they're created, otherwise the operation will revert
-    setTimeout(async () => {
-      await core.createHorse(acc[2], "male hash"); // 2
-    }, 100);
+    await core.createHorse(acc[2], "male hash"); // 2
 
     // This should create another token with other stuff specified.
     await core.mix(2, 1, "female offspring hash", {from: owner}); // 3
@@ -33,17 +28,17 @@ contract("Breeding", acc => {
     assert.equal(secondOffspringStats[0].toNumber(), 1);
 
     let offspringSex = await core.getHorseSex.call(3);
-    assert.equal(offspringSex, "F");
+    assert.equal(web3.toUtf8(offspringSex), "F");
 
     // Ensure the owner is the owner of the female horse
-    let ownerOfToken = await core.ownerOf.call(1);
+    let ownerOfToken = await core.ownerOf.call(3);
     assert.equal(ownerOfToken, owner);
   })
 
   it("should revert when mixing with a horse in the same lineage", async () => {
     // Offspring created in the last test
     try {
-      await core.mix(2, 3, "some hash", {from: acc[2]});
+      await core.mix(2, 3, "failed female hash", {from: acc[2]});
       assert.fail("Expected revert not received");
     } catch(err) {
       let revertFound = err.message.search("revert") >= 0;
@@ -52,16 +47,24 @@ contract("Breeding", acc => {
   })
 
   it("should revert when horses are related in lineages", async () => {
-    // They're in the same block.timestamp here, this reverts the transaction.
-    // i.e. Horses can't breed with horses from the same timestamp neither.
-    await core.createHorse(acc[1], "male hash"); // 4
+    await core.createHorse(owner, "hash"); // 4
+    // By design, horses can't mate with a horse of the same gender.
+    // Horses of the same gender and in the same block will have the same timestamp.
+    // Mating with two horses from the same block isn't possible.
+    await core.mix(4, 3, "female hash", {from: owner}); // 5
 
     try {
-      await core.mix(4, 3, "female hash", {from: owner}); // 5
+      await core.mix(2, 4, "failed female hash", {from: owner}); // Failed
       assert.fail("Expected revert not received");
     } catch(err) {
       let revertFound = err.message.search("revert") >= 0;
       assert(revertFound, `Expected "revert", got ${err} instead`);
     }
+  })
+
+  it("should create a base value for the offspring", async () => {
+    let baseValue = await core.getBaseValue.call(5); 
+    assert.notEqual(baseValue.toNumber(), 0);
+    assert.isBelow(baseValue.toNumber(), 50);
   })
 })
