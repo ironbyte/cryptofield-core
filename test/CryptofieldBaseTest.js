c5cccb0ad663bfcf85beb3a00fbf081a62c689f0
Miguel Diaz <diazmiiguel@gmail.com>
2018-08-22 20:33:16 -0400

    New impl test

--- test/CryptofieldBaseTest.js
+++ test/CryptofieldBaseTest.js
@@ -1,25 +1,33 @@
 const Core = artifacts.require("./Core");
 
 contract("CryptofieldBaseContract", accounts => {
-  let instance;
+  let core;
   let hash = "QmTsG4gGyRYXtBeTY7wqcyoksUp9QUpjzoYNdz8Y91GwoQ";
   let buyer = accounts[1];
+  let defaults = [
+    "Austin Riffle", "Jerri Curl", "Amoxi", "Chase Jackson", "Zeus", "Apollo"
+  ];
 
 
   beforeEach("setup contract instance", async () => {
-    instance = await Core.deployed();
+    core = await Core.deployed();
   })
 
   it("should be able to buy a horse", async () => {
-    await instance.createGOP(buyer, hash);
-    let horse = await instance.getHorse.call(0)
+    await core.createGOP(buyer, hash);
+    let horse = await core.getHorseHash.call(0)
 
     assert.equal(horse, hash);
   })
 
   it("should return the correct hash for a horse", async () => {
-    let returnedHash = await instance.getHorse.call(0);
+    let returnedHash = await core.getHorseHash.call(0);
 
     assert.equal(hash, returnedHash);
+  })
+
+  it("should contain one of the default names for GOP", async () => {
+    let horseName = await core.getHorseName.call(0);
+    assert.include(defaults, horseName);
   })
 })
