36340018d3e7063d530d3529d46d6d976af2175c
Miguel Diaz <diazmiiguel@gmail.com>
2018-09-17 21:53:22 -0400

    fixups

--- truffle.js
+++ truffle.js
@@ -1,7 +1,7 @@
 require("babel-register");
 require("babel-polyfill");
 
-if(process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production") {
   require("dotenv").load();
 }
 
@@ -11,8 +11,8 @@ var mnemonic = process.env.mnemonic
 module.exports = {
   solc: {
     optimizer: {
-        enabled: true,
-        runs: 200
+      enabled: true,
+      runs: 200
     }
   },
   networks: {
@@ -20,22 +20,18 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
-      gas: 6721975,
-      gasPrice: 20000000000
     },
     ropsten: {
       provider: () => {
         return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + process.env.infura_token)
       },
       network_id: 3,
-      gas: 4712388
     },
     kovan: {
       provider: () => {
         return new HDWalletProvider(mnemonic, "https://kovan.infura.io/" + process.env.kovan_token)
       },
       network_id: 4,
-      gas: 4712388
     }
   }
 };
