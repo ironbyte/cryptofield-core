28361d4d640df6387232461a4e3a479c2898c7b0
Miguel Diaz <diazmiiguel@gmail.com>
2018-10-09 12:45:22 -0400

    Creates Races contract

---
+++ contracts/Races.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.4.24;
+
+/*
+@description This contract is in charge of keeping some kind of state and
+handling anything related to payments, everything else is being handled by the API.
+*/
+contract Races {
+}
